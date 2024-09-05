05a93ec40a7c5ff4448ed190a34e0158a0242a70
niveditha321 <146170497+niveditha321@users.noreply.github.com>
2024-09-06 00:24:18 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,9 @@
+
+document.getElementById('welcome-button').addEventListener('click', function() {
+    alert('Hello, welcome to my page!');
+});
+
+
+document.getElementById('profile-pic').addEventListener('mouseleave', function() {
+    document.body.style.backgroundColor = '#f0f8ff';
+});
